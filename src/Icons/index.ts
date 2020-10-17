ad1d6fa10f98232af716f860be4f97a006f6060d
Everton <vertopge@hotmail.com>
2020-10-16 22:43:16 -0300

    export icons

---
+++ src/Icons/index.ts
@@ -0,0 +1,10 @@
+// Ícones Próprios
+import IconPizzaria from './IconPizzaria';
+import IconLanchonete from './IconLanchonete';
+
+// // Ícones Material-ui
+import AddIcon from '@material-ui/icons/Add';
+import VisibilityOutlinedIcon from '@material-ui/icons/VisibilityOutlined';
+import SearchIcon from '@material-ui/icons/Search';
+
+export { IconPizzaria, IconLanchonete, AddIcon, VisibilityOutlinedIcon, SearchIcon };
--- src/Icons/index.tsx
+++
@@ -1,8 +0,0 @@
-// Ícones Próprios
-export { default as IconPizzaria } from './IconPizzaria';
-export { default as IconLanchonete } from './IconLanchonete';
-
-// Ícones Material-ui
-export { default as AddIcon } from '@material-ui/icons/Add';
-export { default as VisibilityOutlinedIcon } from '@material-ui/icons/VisibilityOutlined';
-export { default as SearchIcon } from '@material-ui/icons/Search';
